2dd0515b3d06063578963472a2eab2388285a385
Gregory-Nam <gregory.nam@etu.univ-amu.fr>
2018-10-04 09:13:45 +0200

    implementation des setter et getter dans la classe recette

--- model/Recette.php
+++ model/Recette.php
@@ -2,29 +2,68 @@
 class Recette
 {
 	private $_id;
-	private $_titre;
+	private $_title;
+	private $_description;
+	private $_auteur;
 	//ET TOUTES LES VALEURS QUE L'ON RECUPERER DANS LA TABLE
-	
+
 	//CONSTRUCTEUR
 	public function __construct(array $data)
 	{
 		$this->hydrate($data);
 	}
-	
+
 	public function hydrate(array $data)
 	{
 		foreach($data as $key => $value)
 		{
 			$method = 'set'.ucfirst($key);
-			
+
 			if(method_exists($this, $method))
 				$this->$method($value);
 		}
 	}
-	
+
 	//SETTERS
-		//A IMPLEMENTER SELON LES DONNES MEMBRE
-		
+
+	public function setTitle($name)
+	{
+		if(is_string($name))
+			$this->_title = $name;
+	}
+
+	public function setDescription($name)
+	{
+		if(is_string($name))
+			$this->_title = $name;
+
+	}
+
+	public function setAuteur($name)
+	{
+		if(is_string($name))
+			$this->_title = $name;
+
+	}
+
 	//GETTER
-		//A IMPLEMENTER SELON LES DONNES MEMBRE
+	public function getId()
+	{
+		return $this->_id;
+	}
+
+	public function getTitle()
+	{
+		return $this->_title;
+	}
+
+	public function getDescription()
+	{
+		return $this->_description;
+	}
+
+	public function getTitle()
+	{
+		return $this->_title;
+	}
 }
