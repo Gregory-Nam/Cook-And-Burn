e8303b26b075e6478ec4ef4ad5425c2cfbf73e85
Gregory-Nam <gregory.nam@etu.univ-amu.fr>
2018-10-04 09:04:42 +0200

    modifi de controllerindex.php

--- controler/ControllerIndex.php
+++ controler/ControllerIndex.php
@@ -3,21 +3,21 @@ class ControllerIndex
 {
 	private $_recetteModel;
 	private $_view;
-	
+
 	public function __construct($url)
-	{	
+	{
 	//1 car un seul parametre dans l'URL, dans l'accueil, seulement pour charger le controller
 		if(isset($url) && count($url) > 1)
-			throw new Excepion('Page introuvable');
+			throw new Exception('Page introuvable');
 		else
 			$this->recette();
 	}
-	
+
 	private function recette()
 	{
-		$this->_recetteModel = new ArticleModel;
+		$this->_recetteModel = new TopRecetteModel();
 		$recette = $this->_recetteModel->getRecettes();
-		
+
 		require_once('view/viewAccueil.php');
 	}
 }
