fcd929df0f94925a8b757b30ee5957b71d11ffb6
Xavier MAISSE <37612819+xavier-maisse@users.noreply.github.com>
2018-10-06 20:17:40 +0200

    Ajout de la sécurité et des verifications

--- controler/ControllerSignUpAction.php
+++ controler/ControllerSignUpAction.php
@@ -13,39 +13,57 @@ class ControllerSignUpAction
 
     public function verif()
     {
-        if(empty($_POST['name']) || empty($_POST['password']) || empty($_POST['mail']))
+        if(empty($_POST['name']) || empty($_POST['password']) || empty($_POST['mail'] || empty($_POST['password2'])))
         {
             echo "Il faut tout remplir";
             //header('location:Index');
         }
         else
         {
-            $nameUser = $_POST['name'];
-            $firstNameUser = $_POST['password'];
-            $mailUser = $_POST['mail'];
+            $nameUser = htmlspecialchars($_POST['name']);
+            $firstNameUser = sha1($_POST['password']);
+            $firstNameUser2 = sha1($_POST['password2']);
+            $mailUser = htmlspecialchars($_POST['mail']);
+            $mailUser2 = htmlspecialchars($_POST['mail2']);
             $aUser = new User($nameUser,$firstNameUser, $mailUser);
-
-
-            try
-            {
-                $test = new UserModel();
-                $test->insertUser($aUser);
-
+            $pseudolength = strlen($nameUser);
+            if($pseudolength <= 255){
+                if($firstNameUser == $firstNameUser2){
+                    if($mailUser == $mailUser2){
+                        if(filter_var($mailUser, FILTER_VALIDATE_EMAIL)){
+                            try
+                            {
+                                $test = new UserModel();
+                                $test->insertUser($aUser);
+                            }
+                            catch(PDOException $e)
+                            {
+                                die('Error : ' . $e->getMessage());
+                            }
+                            echo "Inscription réussit !";
+                            echo $aUser->getNameUser();
+                            //header('location:Index');
+                        }
+                        else{
+                            echo "Votre adresse mail n'est pas valide !";
+                        }   
+                    }
+                    else{
+                        echo "Les adresses emails ne correspondent pas !";
+                    }
+                }
+                else{
+                    echo "Vos mots de passes ne correspondent pas !";
+                }      
             }
-            catch(PDOException $e)
-            {
-                die('Error : ' . $e->getMessage());
-            }
-            echo "lol";
-            echo $aUser->getNameUser();
-            //header('location:Index');
+            else{
+                    echo "Votre pseudo ne doit pas dépasser 255 caractères !";
+                }
+            
+   
         }
     }
 }
-
-
-
-
 
 
 
