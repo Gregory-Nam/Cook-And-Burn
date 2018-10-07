19e0dc71fa39b0052e28ea0c90e1dd98c54b3307
Xavier MAISSE <37612819+xavier-maisse@users.noreply.github.com>
2018-10-07 18:42:45 +0200

    ajout du profil

---
+++ view/viewProfil.php
@@ -0,0 +1,14 @@
+<?php
+	session_start();
+    $this->_t = "Profil";
+    ?>
+
+</div>
+</div>
+</div>
+</div>
+
+<?php 
+	echo '<center><h1>'. 'Profil de : ' .$_SESSION['pseudo'] . '</h1></center>';
+
+?>
