1107bd6ee7826b801d3b9b520badb9882a9cb2e4
Xavier MAISSE <37612819+xavier-maisse@users.noreply.github.com>
2018-10-07 17:44:59 +0200

    confirmation mail

---
+++ view/viewConfirmation.php
@@ -0,0 +1,16 @@
+<?php
+    $this->_t = "Inscription";
+ ?>   
+<center><form action="ConfirmationAction" method="post">
+	<div class="contact-form">
+	<p>
+        <input type="text" name="pseudo" placeholder ="Nom utilisateur" id="name"/>
+    </p>
+    <p>
+        <input type="text" name="cleSecrete" placeholder ="Clé secrete" id="name"/>
+    </p>
+	<p>
+        <input type="submit" name="action" value="Je valide mon compte"/>
+    </p>
+</div>
+</form></center>
