ea9fec40e2ee86289a972d5f9346a61b30cc71ba
Xavier MAISSE <37612819+xavier-maisse@users.noreply.github.com>
2018-10-07 18:52:42 +0200

    Ajout de la déconnexion

--- view/viewProfil.php
+++ view/viewProfil.php
@@ -1,6 +1,11 @@
 <?php
 	session_start();
     $this->_t = "Profil";
+
+    if(isset($_POST['deco'])){
+    	session_destroy();
+    	header("Location: connexion");
+    }
     ?>
 
 </div>
@@ -10,5 +15,9 @@
 
 <?php 
 	echo '<center><h1>'. 'Profil de : ' .$_SESSION['pseudo'] . '</h1></center>';
-
 ?>
+<div class="contact-form">
+	<form action="" method="post">
+	<center><input type="submit" name="deco" value="Me Déconnecter"></center>
+	</form>
+</div>
