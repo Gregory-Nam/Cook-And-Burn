48247d9e9d8d08aa8b9fe4ec0dbc8798f9ee8e23
Xavier MAISSE <37612819+xavier-maisse@users.noreply.github.com>
2018-10-09 16:27:57 +0200

    Update Model.php

--- model/Model.php
+++ model/Model.php
@@ -32,7 +32,7 @@ abstract class Model
 	  $req->execute();
 	  while($data = $req->fetch(PDO::FETCH_ASSOC))
 	  {
-		$var[] = new $obj($data['titre'], $data['descritpion'], $data['auteur'], $data['ingredient'],$data['image'], $data['nombre_de_personne']);
+		$var[] = new $obj($data['titre'], $data['descritpion'],$data['descritpionDet'], $data['auteur'], $data['ingredient'],$data['image'], $data['nombre_de_personne']);
 	  }
 	  return $var;
 	  $req->closeCursor();
