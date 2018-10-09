81cdcba23af7f60853e873a7ef61685972558f8f
Xavier MAISSE <37612819+xavier-maisse@users.noreply.github.com>
2018-10-09 10:59:04 +0200

    creation recette

---
+++ view/viewCreationRecette.php
@@ -0,0 +1,30 @@
+<?php
+    $this->_t = "Création recette";
+    ?>
+
+</div>
+</div>
+</div>
+</div>
+    <div class="contact-form">
+                <h3>Ma recette : </h3>
+        <center><form action="RecetteAction" method="post">
+            <p>
+                <input type="text" name="nameRecette" placeholder ="Titre de la recette" />
+            </p>
+
+            <p>
+                <input type="textarea" name="descriptionRecette" placeholder="Description de la recette"/>
+            </p>
+            <p>
+                <input type="file" name="descriptionRecette" placeholder="Description de la recette"/>
+            </p>
+
+            <p>Nombre de personne :</p> 
+                <input type="number" name="action" />
+
+            <p>
+                <input type="submit" rows="10" cols="50" name="action" value="Créer !"/>
+            </p>
+
+</form></center></div>
