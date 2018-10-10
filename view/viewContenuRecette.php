f65e1d467b0e382ce97ed33bad1d71a8f675b62f
NAM Gregory <gregory.nam@etu.univ-amu.fr>
2018-10-10 11:06:26 +0200

    modification vue contenu recette

--- view/viewContenuRecette.php
+++ view/viewContenuRecette.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+$_SESSION['recette'] = substr(strrchr($_SERVER['REQUEST_URI'], '='), 1);
 $titre = substr(strrchr($_SERVER['REQUEST_URI'], '='), 1);
 $this->_t = $titre;?>
     </div>
@@ -19,7 +21,22 @@ $this->_t = $titre;?>
         <h2> Description détaillée </h2>
         <p> <?php echo $marec->getDescriptionDet();?></p> <br/>
         <h2> Ingredients pour <?php echo $marec->getNombrePersonne();?> personnes </h2>
-        <p> <?php echo $marec->getIngredient();?></p> <br/>
+        <p> <?php echo $marec->getIngredient();?></p> <br/><br/><br/>
+        <h1>Commentaires :</h1>
+        <div class="contact-form">
+        <form method="post" action="CommentaireRecette">
+            <input type="textarea" name="commentaireRecette" placeholder="Recette vraiment excellente ! ">
+            <input type="submit" name="action" value="Poster"/>
+        </form>
+        </div>
+        <div class="customer-info">
+            <p><?php echo $um->getCommentaire($titre); ?></p>
+        </div>
+        <div class="contact-form">
+        <form method="post" action="MettreFavoris">
+            <input type="submit" name="action" value="Poster"/>
+        </form>
+        </div>
 
 
     </center>
