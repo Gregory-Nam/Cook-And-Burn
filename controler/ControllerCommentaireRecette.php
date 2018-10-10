002c61b3b908c2d817eeb5d9a6fa98bd6df590a8
Xavier MAISSE <37612819+xavier-maisse@users.noreply.github.com>
2018-10-10 10:49:35 +0200

    ajout commentaire

---
+++ controler/ControllerCommentaireRecette.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+//include('../view/viewSignUp
+//include('./model/User.php');
+//include ('ControllerSignUpAction.php');
+error_reporting(E_ALL);
+ini_set('display-errors','on');
+
+class ControllerCommentaireRecette
+{
+    private $test;
+    public function __construct()
+    {
+        $this->verif();
+    }
+
+    public function verif()
+    {
+    	if(empty($_POST['commentaireRecette'])){
+        echo 'vide';
+      }
+      else{
+        $commentaire = htmlspecialchars($_POST['commentaireRecette']);
+        $test = new RecetteModel();
+        $test->postCommentaire($commentaire,$_SESSION['pseudo'],$_SESSION['recette']);
+        echo 'Commentaire posté';
+      }
+    }
+}
+?>
