69681c12a811844409dbabb84ae2e6d54875f911
Xavier MAISSE <37612819+xavier-maisse@users.noreply.github.com>
2018-10-16 13:13:23 +0200

    Add files via upload

---
+++ controler/ControllerModifUserTableAction.php
@@ -0,0 +1,49 @@
+<?php
+session_start();
+//include('../view/viewSignUp
+//include('./model/User.php');
+//include ('ControllerSignUpAction.php');
+error_reporting(E_ALL);
+ini_set('display-errors','on');
+
+class ControllerModifUserTableAction
+{
+    private $test;
+    public function __construct()
+    {
+        $this->verif();
+    }
+
+    public function verif()
+    {
+        $idPro = $_SESSION['profilSup'];
+        if(empty($_POST['nom']) or empty($_POST['mail']))
+        {
+            $_SESSION['erreur'] = 'Remplir tout les champs !';
+            header("Location:ModifUserTable?id=$idPro");
+        }
+        else
+        {
+            $nom = htmlspecialchars($_POST['nom']);
+            $mail = htmlspecialchars($_POST['mail']);
+            $id = htmlspecialchars($_SESSION['profilSup']);
+            if(filter_var($mail, FILTER_VALIDATE_EMAIL))
+            {
+                $_SESSION['reussi'] = 'Modifié !';
+                header("Location:ModifUserTable?id=$idPro");
+            }
+            else
+            {
+                $_SESSION['erreur'] = 'Mail non valide !';
+                header("Location:ModifUserTable?id=$idPro");
+            }
+            
+        }
+        
+    	
+    	
+    }
+}
+
+?>
+
