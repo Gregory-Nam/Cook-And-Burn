8400185f77e3916cb5f2db6f454466d4a85f157a
Gregory-Nam <gregory.nam@etu.univ-amu.fr>
2018-11-04 15:07:49 +0100

    modif sur les erreurs

---
+++ controler/controler/ControllerIngredientTable.php
@@ -0,0 +1,45 @@
+<?php
+require_once('view/View.php');
+include('./model/User.php');
+include ('./model/UserModel.php');
+require_once ('./model/reCaptcha/autoload.php');
+session_start();
+
+/**
+ * Class ControllerIngredientTable
+ */
+class ControllerIngredientTable
+{
+    private $_userModel;
+    private $_view;
+    private $_recetteModel;
+    public function __construct()
+    {
+        if(isset($url))
+            throw new Exception('Page introuvable');
+        else
+            $this->ingredientTable();
+    }
+
+    /**
+     * Affiche la vue pour gérer les ingrédients
+     * On verifie que l'utilisateur connecté soit bien l'administrateur
+     */
+    public function ingredientTable()
+    {
+        $this->_userModel = new UserModel();
+        $this->_recetteModel = new RecetteModel();
+
+        $this->_ingredientModel = new IngredientsModel();
+        if(isset($_SESSION['pseudo']) AND $_SESSION['pseudo']=='adm'){
+           $this->_view = new View('ingredientsTable'); 
+        }
+        else{
+            header('Location:Error');
+        }
+        $this->_view->generate(array("uM" => $this->_userModel, "rM" => $this->_recetteModel, "in"=> $this->_ingredientModel));
+
+
+
+    }
+}
---
+++ controler/controler/ControllerModifUserRecette.php
@@ -0,0 +1,42 @@
+<?php
+require_once('view/View.php');
+include('./model/User.php');
+include ('./model/UserModel.php');
+include('./model/Recette.php');
+include ('./model/RecetteModel.php');
+require_once ('./model/reCaptcha/autoload.php');
+session_start();
+class ControllerModifUserRecette
+{
+    private $_userModel;
+    private $_recetteModel;
+    private $_ingredientsModel;
+    private $_view;
+    public function __construct()
+    {
+        if(isset($url))
+            throw new Exception('Page introuvable');
+        else
+            $this->modifTable();
+    }
+
+    public function modifTable()
+    {
+
+        $this->_userModel = new UserModel();
+        $this->_recetteModel = new RecetteModel();
+        $this->_ingredientsModel = new IngredientsModel();
+        $categories = $this->_ingredientsModel->getCategories();
+        $ingredients = $this->_ingredientsModel->getAll();
+        if(isset($_SESSION['pseudo']) AND $_SESSION['pseudo']=='adm'){
+           $this->_view = new View('ModifRecetteTable'); 
+        }
+        else{
+            header('Location:Error');
+        }
+        $this->_view->generate(array("ingredients" => $ingredients,"uM" => $this->_userModel, "rM" => $this->_recetteModel,"iM" => $this->_ingredientsModel,"categories" => $categories));
+
+    }
+}
+
+
