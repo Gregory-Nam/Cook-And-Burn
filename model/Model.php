a2b7fc42dd100512144c3f0a9d6cd012ae6a2ae4
Grégory Nam <37612938+Gregory-Nam@users.noreply.github.com>
2019-02-04 22:44:04 +0100

    Update Model.php

--- model/Model.php
+++ model/Model.php
@@ -7,12 +7,12 @@ abstract class Model
 	private static $_bdd;
 	private $servername = "mysql-cookandburn-gxaj.alwaysdata.net";
 	private $username = "167602";
-	private $password = "barbecue2018";
-	private $dbname = "cookandburn-gxaj_g2";
+	private $password = "**********";
+	private $dbname = "**************";
 	//INSTANCIATION DE LA CONNEXION A LA BDD
 	private static function setBdd()
 	{
-		self::$_bdd = new PDO('mysql:host=mysql-cookandburn-gxaj.alwaysdata.net;dbname=cookandburn-gxaj_g2;charset=utf8','167602', 'barbecue2018');
+		self::$_bdd = new PDO('mysql:host=$servername;dbname=$dbname;charset=utf8',$username, $password);
 		//self::$_bdd->setAtribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 	}
 
